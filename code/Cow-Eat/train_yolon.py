50874a65be45660373e4c0d8ef748fb6d08f296e
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 05:09:23 +0000

    Auto-format by https://ultralytics.com/actions

--- code/Cow-Eat/train_yolon.py
+++ code/Cow-Eat/train_yolon.py
@@ -1,17 +1,17 @@
 from ultralytics import YOLO
 
+
 def train():
     # Load a pretrained YOLO12n model
     model = YOLO("datasets/Cow-eat-chew-behaviour/yolo26n_8.yaml")
     # model = YOLO("runs/detect/Cow-EatOrChew-train-yolo26n-C3k2Star/weights/best.pt")
 
     # Train the model  for  epochs
-    train_results = model.train(
+    model.train(
         data="datasets/Cow-eat-chew-behaviour/CHVI.yaml",  # Path to dataset configuration file
         epochs=300,  # Number of training epochs
         imgsz=640,  # Image size for training
         device="0",  # Device to run on (e.g., 'cpu', 0, [0,1,2,3])
-
         mpdiou=False,  # Use MPDIoU loss for bounding box regression
         # optimizer="AdamW",  # Optimizer to use (e.g., 'SGD', 'Adam', 'AdamW')
         resume=True,
@@ -19,25 +19,27 @@ def train():
         # patience=50,
         batch=16,
         name="Cow-EatOrChew-train-yolo26n-MSAC2PSA",  # Name for the training run, used for saving results
-        save_period=20, # 保存模型检查点的频率，以 epoch 为单位指定。值为 -1 时禁用此功能。适用于在长时间训练期间保存临时模型。
-        plots=True # 生成并保存训练和验证指标的图表，以及预测示例，从而 提供对模型性能和学习进度的可视化见解
+        save_period=20,  # 保存模型检查点的频率，以 epoch 为单位指定。值为 -1 时禁用此功能。适用于在长时间训练期间保存临时模型。
+        plots=True,  # 生成并保存训练和验证指标的图表，以及预测示例，从而 提供对模型性能和学习进度的可视化见解
     )
 
     # calculate average FPS after training
     fps_list = [[], [], [], []]
     for ep in range(10):
-        metrics = model.val(project='runs/detect/Cow-EatOrChew-train-yolo26n-MSAC2PSA',
-                            data="datasets/Cow-eat-chew-behaviour/CHVI.yaml",
-                            imgsz=640,
-                            batch=16,
-                            device="0"
-                            )
-        fps_list[0].append(metrics.speed['preprocess'])
-        fps_list[1].append(metrics.speed['inference'])
-        fps_list[2].append(metrics.speed['postprocess'])
-        fps_list[3].append(metrics.speed['preprocess'] + metrics.speed['inference'] + metrics.speed['postprocess'])
-    print("FPS (preprocess): {:.2f}\nFPS (inference): {:.2f}\nFPS (postprocess): {:.2f}\nFPS (total): {:.2f}\nFPS (MAX): {:.2f}".format(1000 / (sum(fps_list[0]) / 10), 1000 / (sum(fps_list[1]) / 10), 1000 / (sum(fps_list[2]) / 10), 1000 / (sum(fps_list[3]) / 10) , 1000 / min(fps_list[3]) ))
-    
+        metrics = model.val(
+            project="runs/detect/Cow-EatOrChew-train-yolo26n-MSAC2PSA",
+            data="datasets/Cow-eat-chew-behaviour/CHVI.yaml",
+            imgsz=640,
+            batch=16,
+            device="0",
+        )
+        fps_list[0].append(metrics.speed["preprocess"])
+        fps_list[1].append(metrics.speed["inference"])
+        fps_list[2].append(metrics.speed["postprocess"])
+        fps_list[3].append(metrics.speed["preprocess"] + metrics.speed["inference"] + metrics.speed["postprocess"])
+    print(
+        f"FPS (preprocess): {1000 / (sum(fps_list[0]) / 10):.2f}\nFPS (inference): {1000 / (sum(fps_list[1]) / 10):.2f}\nFPS (postprocess): {1000 / (sum(fps_list[2]) / 10):.2f}\nFPS (total): {1000 / (sum(fps_list[3]) / 10):.2f}\nFPS (MAX): {1000 / min(fps_list[3]):.2f}"
+    )
 
     # Evaluate the model's performance on the validation set
     # metrics = model.val(project='runs/detect/Cow-EatOrChew-train-yolo26n',
@@ -46,7 +48,7 @@ def train():
     #                     batch=1,
     #                     device="0"
     #                     )
-    
+
     # metrics.box.map  # map50-95
     # metrics.box.map50  # map50
     # metrics.box.map75  # map75
--- ultralytics/nn/new_modules/Backbone_RepViT.py
+++ ultralytics/nn/new_modules/Backbone_RepViT.py
@@ -1,14 +1,14 @@
-import torch.nn as nn
 import numpy as np
-from timm.models.layers import SqueezeExcite
 import torch
+import torch.nn as nn
+from timm.models.layers import SqueezeExcite
 
-__all__ = ['repvit_m0_9', 'repvit_m1_0', 'repvit_m1_1', 'repvit_m1_5', 'repvit_m2_3']
+__all__ = ["repvit_m0_9", "repvit_m1_0", "repvit_m1_1", "repvit_m1_5", "repvit_m2_3"]
 
 
 def replace_batchnorm(net):
     for child_name, child in net.named_children():
-        if hasattr(child, 'fuse_self'):
+        if hasattr(child, "fuse_self"):
             fused = child.fuse_self()
             setattr(net, child_name, fused)
             replace_batchnorm(fused)
@@ -39,12 +39,10 @@ def _make_divisible(v, divisor, min_value=None):
 
 
 class Conv2d_BN(torch.nn.Sequential):
-    def __init__(self, a, b, ks=1, stride=1, pad=0, dilation=1,
-                 groups=1, bn_weight_init=1, resolution=-10000):
+    def __init__(self, a, b, ks=1, stride=1, pad=0, dilation=1, groups=1, bn_weight_init=1, resolution=-10000):
         super().__init__()
-        self.add_module('c', torch.nn.Conv2d(
-            a, b, ks, stride, pad, dilation, groups, bias=False))
-        self.add_module('bn', torch.nn.BatchNorm2d(b))
+        self.add_module("c", torch.nn.Conv2d(a, b, ks, stride, pad, dilation, groups, bias=False))
+        self.add_module("bn", torch.nn.BatchNorm2d(b))
         torch.nn.init.constant_(self.bn.weight, bn_weight_init)
         torch.nn.init.constant_(self.bn.bias, 0)
 
@@ -53,27 +51,34 @@ class Conv2d_BN(torch.nn.Sequential):
         c, bn = self._modules.values()
         w = bn.weight / (bn.running_var + bn.eps) ** 0.5
         w = c.weight * w[:, None, None, None]
-        b = bn.bias - bn.running_mean * bn.weight / \
-            (bn.running_var + bn.eps) ** 0.5
-        m = torch.nn.Conv2d(w.size(1) * self.c.groups, w.size(
-            0), w.shape[2:], stride=self.c.stride, padding=self.c.padding, dilation=self.c.dilation,
-                            groups=self.c.groups,
-                            device=c.weight.device)
+        b = bn.bias - bn.running_mean * bn.weight / (bn.running_var + bn.eps) ** 0.5
+        m = torch.nn.Conv2d(
+            w.size(1) * self.c.groups,
+            w.size(0),
+            w.shape[2:],
+            stride=self.c.stride,
+            padding=self.c.padding,
+            dilation=self.c.dilation,
+            groups=self.c.groups,
+            device=c.weight.device,
+        )
         m.weight.data.copy_(w)
         m.bias.data.copy_(b)
         return m
 
 
 class Residual(torch.nn.Module):
-    def __init__(self, m, drop=0.):
+    def __init__(self, m, drop=0.0):
         super().__init__()
         self.m = m
         self.drop = drop
 
     def forward(self, x):
         if self.training and self.drop > 0:
-            return x + self.m(x) * torch.rand(x.size(0), 1, 1, 1,
-                                              device=x.device).ge_(self.drop).div(1 - self.drop).detach()
+            return (
+                x
+                + self.m(x) * torch.rand(x.size(0), 1, 1, 1, device=x.device).ge_(self.drop).div(1 - self.drop).detach()
+            )
         else:
             return x + self.m(x)
 
@@ -81,14 +86,14 @@ class Residual(torch.nn.Module):
     def fuse_self(self):
         if isinstance(self.m, Conv2d_BN):
             m = self.m.fuse_self()
-            assert (m.groups == m.in_channels)
+            assert m.groups == m.in_channels
             identity = torch.ones(m.weight.shape[0], m.weight.shape[1], 1, 1)
             identity = torch.nn.functional.pad(identity, [1, 1, 1, 1])
             m.weight += identity.to(m.weight.device)
             return m
         elif isinstance(self.m, torch.nn.Conv2d):
             m = self.m
-            assert (m.groups != m.in_channels)
+            assert m.groups != m.in_channels
             identity = torch.ones(m.weight.shape[0], m.weight.shape[1], 1, 1)
             identity = torch.nn.functional.pad(identity, [1, 1, 1, 1])
             m.weight += identity.to(m.weight.device)
@@ -120,8 +125,9 @@ class RepVGGDW(torch.nn.Module):
 
         conv1_w = torch.nn.functional.pad(conv1_w, [1, 1, 1, 1])
 
-        identity = torch.nn.functional.pad(torch.ones(conv1_w.shape[0], conv1_w.shape[1], 1, 1, device=conv1_w.device),
-                                           [1, 1, 1, 1])
+        identity = torch.nn.functional.pad(
+            torch.ones(conv1_w.shape[0], conv1_w.shape[1], 1, 1, device=conv1_w.device), [1, 1, 1, 1]
+        )
 
         final_conv_w = conv_w + conv1_w + identity
         final_conv_b = conv_b + conv1_b
@@ -132,8 +138,7 @@ class RepVGGDW(torch.nn.Module):
         bn = self.bn
         w = bn.weight / (bn.running_var + bn.eps) ** 0.5
         w = conv.weight * w[:, None, None, None]
-        b = bn.bias + (conv.bias - bn.running_mean) * bn.weight / \
-            (bn.running_var + bn.eps) ** 0.5
+        b = bn.bias + (conv.bias - bn.running_mean) * bn.weight / (bn.running_var + bn.eps) ** 0.5
         conv.weight.data.copy_(w)
         conv.bias.data.copy_(b)
         return conv
@@ -141,38 +146,42 @@ class RepVGGDW(torch.nn.Module):
 
 class RepViTBlock(nn.Module):
     def __init__(self, inp, hidden_dim, oup, kernel_size, stride, use_se, use_hs):
-        super(RepViTBlock, self).__init__()
+        super().__init__()
         assert stride in [1, 2]
 
         self.identity = stride == 1 and inp == oup
-        assert (hidden_dim == 2 * inp)
+        assert hidden_dim == 2 * inp
 
         if stride == 2:
             self.token_mixer = nn.Sequential(
                 Conv2d_BN(inp, inp, kernel_size, stride, (kernel_size - 1) // 2, groups=inp),
                 SqueezeExcite(inp, 0.25) if use_se else nn.Identity(),
-                Conv2d_BN(inp, oup, ks=1, stride=1, pad=0)
+                Conv2d_BN(inp, oup, ks=1, stride=1, pad=0),
             )
-            self.channel_mixer = Residual(nn.Sequential(
-                # pw
-                Conv2d_BN(oup, 2 * oup, 1, 1, 0),
-                nn.GELU() if use_hs else nn.GELU(),
-                # pw-linear
-                Conv2d_BN(2 * oup, oup, 1, 1, 0, bn_weight_init=0),
-            ))
+            self.channel_mixer = Residual(
+                nn.Sequential(
+                    # pw
+                    Conv2d_BN(oup, 2 * oup, 1, 1, 0),
+                    nn.GELU() if use_hs else nn.GELU(),
+                    # pw-linear
+                    Conv2d_BN(2 * oup, oup, 1, 1, 0, bn_weight_init=0),
+                )
+            )
         else:
-            assert (self.identity)
+            assert self.identity
             self.token_mixer = nn.Sequential(
                 RepVGGDW(inp),
                 SqueezeExcite(inp, 0.25) if use_se else nn.Identity(),
             )
-            self.channel_mixer = Residual(nn.Sequential(
-                # pw
-                Conv2d_BN(inp, hidden_dim, 1, 1, 0),
-                nn.GELU() if use_hs else nn.GELU(),
-                # pw-linear
-                Conv2d_BN(hidden_dim, oup, 1, 1, 0, bn_weight_init=0),
-            ))
+            self.channel_mixer = Residual(
+                nn.Sequential(
+                    # pw
+                    Conv2d_BN(inp, hidden_dim, 1, 1, 0),
+                    nn.GELU() if use_hs else nn.GELU(),
+                    # pw-linear
+                    Conv2d_BN(hidden_dim, oup, 1, 1, 0, bn_weight_init=0),
+                )
+            )
 
     def forward(self, x):
         return self.channel_mixer(self.token_mixer(x))
@@ -180,14 +189,17 @@ class RepViTBlock(nn.Module):
 
 class RepViT(nn.Module):
     def __init__(self, cfgs):
-        super(RepViT, self).__init__()
+        super().__init__()
         # setting of inverted residual blocks
         self.cfgs = cfgs
 
         # building first layer
         input_channel = self.cfgs[0][2]
-        patch_embed = torch.nn.Sequential(Conv2d_BN(3, input_channel // 2, 3, 2, 1), torch.nn.GELU(),
-                                          Conv2d_BN(input_channel // 2, input_channel, 3, 2, 1))
+        patch_embed = torch.nn.Sequential(
+            Conv2d_BN(3, input_channel // 2, 3, 2, 1),
+            torch.nn.GELU(),
+            Conv2d_BN(input_channel // 2, input_channel, 3, 2, 1),
+        )
         layers = [patch_embed]
         # building inverted residual blocks
         block = RepViTBlock
@@ -221,14 +233,12 @@ def update_weight(model_dict, weight_dict):
             temp_dict[k] = v
             idx += 1
     model_dict.update(temp_dict)
-    print(f'loading weights... {idx}/{len(model_dict)} items')
+    print(f"loading weights... {idx}/{len(model_dict)} items")
     return model_dict
 
 
-def repvit_m0_9(weights=''):
-    """
-    Constructs a MobileNetV3-Large model
-    """
+def repvit_m0_9(weights=""):
+    """Constructs a MobileNetV3-Large model."""
     cfgs = [
         # k, t, c, SE, HS, s
         [3, 2, 48, 1, 0, 1],
@@ -256,18 +266,16 @@ def repvit_m0_9(weights=''):
         [3, 2, 192, 0, 1, 1],
         [3, 2, 384, 0, 1, 2],
         [3, 2, 384, 1, 1, 1],
-        [3, 2, 384, 0, 1, 1]
+        [3, 2, 384, 0, 1, 1],
     ]
     model = RepViT(cfgs)
     if weights:
-        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)['model']))
+        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)["model"]))
     return model
 
 
-def repvit_m1_0(weights=''):
-    """
-    Constructs a MobileNetV3-Large model
-    """
+def repvit_m1_0(weights=""):
+    """Constructs a MobileNetV3-Large model."""
     cfgs = [
         # k, t, c, SE, HS, s
         [3, 2, 56, 1, 0, 1],
@@ -295,18 +303,16 @@ def repvit_m1_0(weights=''):
         [3, 2, 224, 0, 1, 1],
         [3, 2, 448, 0, 1, 2],
         [3, 2, 448, 1, 1, 1],
-        [3, 2, 448, 0, 1, 1]
+        [3, 2, 448, 0, 1, 1],
     ]
     model = RepViT(cfgs)
     if weights:
-        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)['model']))
+        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)["model"]))
     return model
 
 
-def repvit_m1_1(weights=''):
-    """
-    Constructs a MobileNetV3-Large model
-    """
+def repvit_m1_1(weights=""):
+    """Constructs a MobileNetV3-Large model."""
     cfgs = [
         # k, t, c, SE, HS, s
         [3, 2, 64, 1, 0, 1],
@@ -332,18 +338,16 @@ def repvit_m1_1(weights=''):
         [3, 2, 256, 0, 1, 1],
         [3, 2, 512, 0, 1, 2],
         [3, 2, 512, 1, 1, 1],
-        [3, 2, 512, 0, 1, 1]
+        [3, 2, 512, 0, 1, 1],
     ]
     model = RepViT(cfgs)
     if weights:
-        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)['model']))
+        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)["model"]))
     return model
 
 
-def repvit_m1_5(weights=''):
-    """
-    Constructs a MobileNetV3-Large model
-    """
+def repvit_m1_5(weights=""):
+    """Constructs a MobileNetV3-Large model."""
     cfgs = [
         # k, t, c, SE, HS, s
         [3, 2, 64, 1, 0, 1],
@@ -387,18 +391,16 @@ def repvit_m1_5(weights=''):
         [3, 2, 512, 1, 1, 1],
         [3, 2, 512, 0, 1, 1],
         [3, 2, 512, 1, 1, 1],
-        [3, 2, 512, 0, 1, 1]
+        [3, 2, 512, 0, 1, 1],
     ]
     model = RepViT(cfgs)
     if weights:
-        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)['model']))
+        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)["model"]))
     return model
 
 
-def repvit_m2_3(weights=''):
-    """
-    Constructs a MobileNetV3-Large model
-    """
+def repvit_m2_3(weights=""):
+    """Constructs a MobileNetV3-Large model."""
     cfgs = [
         # k, t, c, SE, HS, s
         [3, 2, 80, 1, 0, 1],
@@ -462,11 +464,12 @@ def repvit_m2_3(weights=''):
     ]
     model = RepViT(cfgs)
     if weights:
-        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)['model']))
+        model.load_state_dict(update_weight(model.state_dict(), torch.load(weights)["model"]))
     return model
 
+
 ############################# 测试 #############################
-if __name__ == '__main__':
+if __name__ == "__main__":
     model = repvit_m2_3()
     inputs = torch.randn((1, 3, 640, 640))
     res = model(inputs)
